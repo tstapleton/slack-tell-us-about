004bd16753159483afc7605ec37f3e5fd862a995
Thomas Stapleton <tst@tradeshift.com>
2021-02-10 22:38:15 -0800

    add todos, better logs, move weekend prompts

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # slack-tell-us-about
 
 Slack app for #tell-us-about
+
+## todo
+
+-   confirm posting daily works - wait until sunday-ish
+-   make data file of prompts
+-   look at tradeshift bots re completeness, permissions
--- src/index.ts
+++ src/index.ts
@@ -14,42 +14,42 @@ const client = new WebClient(process.env.SLACK_BOT_TOKEN, {
 	logLevel: LogLevel.DEBUG,
 });
 
-
 const formatDate = (dateTime: string | number | Date): string => {
 	const dateObj = new Date(dateTime);
 	const year = dateObj.getFullYear();
 	const month = `0${dateObj.getMonth() + 1}`.slice(-2);
 	const date = `0${dateObj.getDate()}`.slice(-2);
 	return `${year}-${month}-${date}`;
-}
+};
 
-const formatPrompt = (prompt: Prompt) => `*TUA${prompt.number}: ${prompt.prompt}*`;
-
-const getPrompt = async (file: string) => {
+const getPrompt = async (file: string, date: string) => {
 	const data = await readFile(file, 'utf-8');
-	const prompts = JSON.parse(data);
+	const prompts: Record<string, Prompt> = JSON.parse(data);
 
-	const now = new Date();
-	const today = formatDate(now);
-
-	const prompt = prompts[today];
-	return formatPrompt(prompt);
-}
+	const prompt = prompts[date];
+	return prompt ? `TUA${prompt.number}: ${prompt.prompt}` : '';
+};
 
 (async () => {
 	try {
-		const prompt = await getPrompt(FILE);
-		const messageResult = await client.chat.postMessage({
-			channel: channelId,
-			text: `${prompt}`,
-		});
-		console.log(messageResult);
+		const today = formatDate(new Date());
+		const prompt = await getPrompt(FILE, today);
 
-		const topicResult = await client.conversations.setTopic({
+		if (!prompt) {
+			console.log(`${today} No prompt today.`);
+			return;
+		}
+
+		await client.chat.postMessage({
 			channel: channelId,
-			topic: `${prompt}`
+			text: `*${prompt}*`,
 		});
-		console.log(topicResult);
+		await client.conversations.setTopic({
+			channel: channelId,
+			topic: `${prompt}`,
+		});
+
+		console.log(`${today} ${prompt}!!!`);
 	} catch (error) {
 		console.error(JSON.stringify(error));
 	}
