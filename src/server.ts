2fa76b284a96beef0ee53322de8b4a52441cfde4
Thomas Stapleton <tst@tradeshift.com>
2021-11-30 23:08:37 -0800

    add pirate quote on start up and update jest config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import app from 'src/app';
 import config from 'src/config';
+import { getQuote } from 'src/util/pirate';
 
 app.listen(config.PORT, async () => {
 	console.info(`Running on http://localhost:${config.PORT}`);
+	const quote = await getQuote();
+	console.info(quote);
 });
 
 process.on('unhandledRejection', (error: Readonly<Error>) => {
